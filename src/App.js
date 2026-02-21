ea2f564ca72f6f0061e7f38adc26d3e2a3bd7b9b
Rohini <rohinigunjal115@gmail.com>
2026-02-21 21:26:43 +0530

    xmedify

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { Outlet } from 'react-router-dom';
 import './App.css';
-import NavBar from "./Components/NavBar/NavBar";
+//import NavBar from "./Components/NavBar/NavBar";
 import Footer from "./Components/Footer/Footer";
 import DownloadApp from "./Components/Sections/DownloadApp/DownloadApp";
 import CssBaseline from '@mui/material/CssBaseline';
--- src/Components/Calendar/TimeSlotPicker/TimeSlotPicker.jsx
+++ src/Components/Calendar/TimeSlotPicker/TimeSlotPicker.jsx
@@ -1,4 +1,4 @@
-import styled from "@emotion/styled";
+//import styled from "@emotion/styled";
 import { Chip, Stack, Typography, Divider } from "@mui/material";
 
 function TimeSlotPicker({
